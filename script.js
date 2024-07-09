55b6f994179bfc3fcf6cf296a009cf01e1df49c7
esh34c <134635429+eshiiiac@users.noreply.github.com>
2024-07-09 14:14:51 +0545

    Update info

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var text = "Front End Web-Dev";
+var text = "Go Lang/API Dev";
 
 const typewriter = document.getElementById('role');
 function typeWriter() {
@@ -16,19 +16,3 @@ typeWriter();
 
 
 
-// for(i=0, i<=5, i++){
-
-//     let roles = ["Student","Front End Web-Dev", "learner","Tech Enthusiast", " Learner", "Video Editor"];
-
-//     var text = `${roles}`;
-
-//     const typewriter = document.getElementById('role');
-//     function typeWriter() {
-//       typewriter.innerHTML = '';
-//       for (let i = 0; i < text.length; i++) {   
-//         setTimeout(() => {     
-//           typewriter.innerHTML += text.charAt(i);
-//         }, 100 * i); 
-//       }
-//     }
-// }
